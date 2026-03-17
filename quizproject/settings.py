91844abaa847be724ed16ed257e42d7396407ac1
kehinde samuel <ayosam207@gmail.com>
2026-03-17 08:59:00 -0700

    django project

--- quizproject/settings.py
+++ quizproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8g6@sqvh&0v#jh246%tlz$ul9j1oa7#(qx)eb)_$o#b$5lzkl3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
